ea0f4435d172129840c79eaece7c802c99cefa67
solahidris <emailsolah@gmail.com>
2023-03-09 23:55:07 +0800

    connect features

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -33,6 +33,7 @@ const AvailableMeals = () => {
   const mealsList = DUMMY_MEALS.map((meal) => (
     <MealItem
       key={meal.id}
+      if={meal.id}
       name={meal.name}
       description={meal.description}
       price={meal.price}
